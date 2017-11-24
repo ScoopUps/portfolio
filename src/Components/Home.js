e14be25002624ed2832fe9d6a20b980e100bcbad
Jason Soukup <jrsoukup17@hotmail.com>
2017-11-23 21:51:33 -0500

    refactored home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,8 +5,7 @@ import photo from '../Images/main-photo.jpg';
 class Home extends Component {
 	render(){
 		return(
-		<div className="main">
-		  <img src={photo} className="main-photo" />
+		<div className="main-home">
 		  <div className="mast">
           	<h1 className="masthead"><span className="retro-effect">J</span><span className="retro-effect">a</span><span className="retro-effect">s</span><span className="retro-effect">o</span><span className="retro-effect">n</span> <span className="retro-effect">S</span><span className="retro-effect">o</span><span className="retro-effect">u</span><span className="retro-effect">k</span><span className="retro-effect">u</span><span className="retro-effect">p</span></h1>
 			<br/>	  
