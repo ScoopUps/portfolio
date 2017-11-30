cc106134f6eff978c8399bdfc6405d07e1bcab6c
Jason Soukup <jrsoukup17@hotmail.com>
2017-11-30 15:12:25 -0500

    work/home page reorg

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
 		  <div className="mast">
           	<h1 className="masthead"><span className="retro-effect">J</span><span className="retro-effect">a</span><span className="retro-effect">s</span><span className="retro-effect">o</span><span className="retro-effect">n</span> <span className="retro-effect">S</span><span className="retro-effect">o</span><span className="retro-effect">u</span><span className="retro-effect">k</span><span className="retro-effect">u</span><span className="retro-effect">p</span></h1>
 			<br/>	  
-		  	<span className="mastsubhead">Web Developer</span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<span className="mastsubhead">Editor</span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<span className="mastsubhead">Project Manager</span> 
+		  	<span className="mastsubhead mastsubhead-1">WEB DEVELOPER</span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<span className="mastsubhead mastsubhead-2">EDITOR</span>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<span className="mastsubhead mastsubhead-3">PROJECT MANAGER</span> 
 		  </div>	
 		</div>
         );
--- src/Components/Work.js
+++ src/Components/Work.js
@@ -7,7 +7,12 @@ class Work extends Component {
 		<div className="Work">
 			<span className="route-header" >WORK</span>
 			<div className="route-container-work">
-				<div className="work-photo-1 work-photo">
+				<div className="work-photo-container">
+					<div className="work-photo-1 work-photo"></div>
+					<div className="work-photo-2 work-photo"></div>
+					<div className="work-photo-3 work-photo"></div>
+					<div className="work-photo-4 work-photo"></div>
+				</div>
 				</div>
 				<div className="work-text">
 					<span className="work-text-header">Bleacher Report</span>
@@ -17,8 +22,7 @@ class Work extends Component {
 					<p className="work-text-body">Served​ ​as​ ​national​ ​editorial​ ​presence​ ​for​ ​several​ ​Bleacher​ ​Report​ ​properties​ ​engaging​ ​millions.​ ​Strategized with​ ​programming,​ ​social​ ​and​ ​analytics​ ​teams​ ​across​ ​the​ ​globe​ ​to​ ​plan​ ​content​ ​budgets,​ ​target​ ​readership and​ ​maximize​ ​impact.​ ​Synced​ ​with​ ​remote​ ​freelance​ ​writing​ ​resources​ ​and​ ​B/R​ ​brand​ ​talent​ ​to​ ​execute original​ ​MLB​ ​content</p>
 					<Link to="/gallery" className="gallery-link">See Gallery</Link>
 				</div>
-				<div className="work-photo-2 work-photo">
-				</div>
+				
 				<div className="work-text">
 					<span className="work-text-header">MLB Advanced Media</span>
 					<span className="work-text-subheader">New York, NY</span>
@@ -26,8 +30,7 @@ class Work extends Component {
 					<span className="title">Site Manager</span>
 					<p className="work-text-body">Executed​ ​MLB.com​ ​site​ ​operations​ ​on​ ​content​ ​management​ ​systems​ ​for​ ​real-time​ ​web/mobile​ ​distribution. Processed​ ​and​ ​deployed​ ​video​ ​highlight​ ​packages​ ​to​ ​MLBAM’s​ ​international​ ​partner​ ​stakeholders.​ ​Oversaw​ ​copy and​ ​style​ ​standards​ ​as​ ​lead​ ​editor​ ​for​ ​the​ ​multimedia​ ​publishing​ ​group.​ ​Progressed​ ​responsibilities​ ​rapidly​ ​from video​ ​highlight​ ​publishing​ ​to​ ​key​ ​web​ ​management​ ​and​ ​lead​ ​editing​ ​roles​ ​in​ ​the​ ​span​ ​of​ ​weeks​ ​after​ ​initial​ ​hire.</p>
 				</div>
-				<div className="work-photo-3 work-photo">
-				</div>
+				
 				<div className="work-text">
 					<span className="work-text-header">National Football League</span>
 					<span className="work-text-subheader">New York, NY</span>
@@ -36,8 +39,7 @@ class Work extends Component {
 					<span className="title">Project Lead</span>
 					<p className="work-text-body">Guided​ ​auditing​ ​operations​ ​for​ ​the​ ​NFL’s​ ​Player​ ​Participation​ ​project,​ ​including​ ​logistics,​ ​quality​ ​assurance​ ​and​ ​data management.​ ​Composed internal corporate communications, such as the <a href="http://www.nflgsis.com/pp/Newsletters/Newsletter2012-01.pdf" target="_blank" rel="noopener noreferrer" className="newsletter-link">Player ​Participation​ Newsletter</a>. Supervised​ ​a​ ​team​ ​of​ ​QA​ ​analysts​ ​and​ ​trained​ ​new​ ​hires​ ​on​ ​project​ ​hardware/software.  Administered​ ​video​ ​operations​ ​and​ ​capture​ ​for​ ​the​ ​league’s​ ​internal​ ​video​ ​system.​ ​Chosen​ ​to​ ​serve​ ​as​ ​on-site, live-event​ ​administrator​ ​for​ ​new​ ​player​ ​safety​ ​protocol​ ​implemented​ ​at​ ​the​ ​2011​ ​AFC​ ​Divisional​ ​Playoffs.</p>
 				</div>
-				<div className="work-photo-4 work-photo">
-				</div>
+				
 				<div className="work-text">
 					<span className="work-text-header">Screenvision</span>
 					<span className="work-text-subheader">New York, NY</span>
@@ -47,7 +49,7 @@ class Work extends Component {
 					<span className="title">Sales Proposal Coordinator</span>
 					<p className="work-text-body">Oversaw​ ​all​ ​aspects​ ​of​ ​national​ ​and​ ​regional​ ​cinema​ ​advertising​ ​campaign​ ​management,​ ​including​ ​scheduling, trafficking​ ​and​ ​inventory.​ ​Projected​ ​sales​ ​impression​ ​levels​ ​and​ ​ensured​ ​campaign​ ​delivery​ ​to​ ​client​ ​fulfillment. Performed​ ​complex​ ​queries​ ​to​ ​optimize​ ​production​ ​costs​ ​and​ ​report​ ​monthly​ ​flight​ ​metrics/trends.​ ​Team​ ​lead​ ​for generating​ ​targeted​ ​client​ ​sales/marketing​ ​proposals​ ​across​ ​all​ ​levels​ ​of​ ​business.​ ​Consulted​ ​senior​ ​management​ ​and systems​ ​developers​ ​on​ ​improving​ ​current​ ​processes​ ​and​ ​integrating​ ​new​ ​hires.​ ​Developed​ ​new​ ​workflows​ ​and​ ​excel tools​ ​to​ ​validate​ ​key​ ​operational​ ​components.​</p>
 				</div>
-			</div>
+			
 		</div>
         );
 	}
